ebea7e75af83436e71071cdb1c6551530fb93e0c
SkutM <ssm011403@gmail.com>
2024-12-05 00:54:14 -0800

    help
    help

--- Prototype01/Assets/EnemyAI.cs
+++ Prototype01/Assets/EnemyAI.cs
@@ -1,22 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class DisplayNumHoops : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI hoopNum;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        hoopNum.text = "x " + GameManager.Instance.NumHoops.ToString();
+        
     }
 }
